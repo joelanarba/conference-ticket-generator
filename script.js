6dd9d46c3213f31a83cf2498cb4c67697d8e910c
Joel Anarba <157653692+joelanarba@users.noreply.github.com>
2025-04-15 22:21:33 +0000

    Update Conference Ticket Generator Project

--- script.js
+++ script.js
@@ -3,19 +3,34 @@
 
 const uploadError = document.getElementById('uploadError');
 
+function validName(name){
+    if (!name) {
+        nameError.innerHTML = `<img src="assets/images/icon-info.svg" class="info" alt=""> Please enter your full name.`;
+        nameError.classList.add('error');
+        
+    } else {
+        fullNameError.innerHTML = '';
+        fullNameError.classList.remove('error');
+
+    }
+}
 function generateTicket(event) {
     event.preventDefault();
 
     const avatar = document.getElementById('avatar').files[0];
     const uploadError = document.getElementById('uploadError');
     const email = document.getElementById('email').value.trim();
-    const fullName = document.getElementById('fullName').value.trim();
+    const fullName = document.getElementById('fullName').value;
     const userName = document.getElementById('github').value.trim();
+    // const displayError = document.querySelectorAll('.display-error');
+    const fullNameError = document.getElementById('name-error');
+    const emailError = document.querySelector('.email-error')
+    const userNameError = document.querySelector('.userName-error')
 
     if (!avatar) {
         uploadError.innerHTML = `<img src="assets/images/icon-info.svg" class="info" alt=""> Please upload an image (JPG or PNG, max 500KB).`;
         uploadError.classList.add('error');
-        return;
+        // return;
     } 
     
     if(avatar.size > 500000){
@@ -29,4 +44,8 @@ function generateTicket(event) {
     }
 
     // Other logic here...
-}
+
+   
+    validName(fullName);
+};
+
